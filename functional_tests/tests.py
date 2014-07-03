cf86185265e2b81c93b87b8c82aefd8a58f320cc
Gareth Arch <garch@qapable.com>
2014-07-03 17:18:13 -0400

    first steps of FT for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -17,6 +17,7 @@ class NewVisitorTest(LiveServerTestCase):
 		self.assertIn(row_text, [row.text for row in rows])
 		
 	def test_can_start_a_list_and_retrieve_it_later(self):
+	
 		# Edith has heard about a cool new online to-do app. She goes
 		# to check out its homepage
 		self.browser.get(self.live_server_url)
@@ -83,6 +84,19 @@ class NewVisitorTest(LiveServerTestCase):
 		page_text = self.browser.find_element_by_tag_name('body').text
 		self.assertNotIn('Buy peacock feathers', page_text)
 		self.assertIn('Buy milk', page_text)
-		
-		
 		# Satisfied, they both go back to sleep
+		
+	def test_layout_add_styling(self):
+		#Edith goes to the home page
+		self.browser.get(self.live_server_url)
+		self.browser.set_window_size(1024, 768)
+		
+		# She starts a new list and sees the input is nicely centered
+		# there too
+		inputbox.send_keys('testing\n')
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(
+			inputbox.location['x'] + inputbox.size['width'] / 2,
+			512,
+			delta=7
+		)
